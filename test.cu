f8075126f53e882974b52d6e2c3314f8260811a3
Methos <lefty.amperiadis@outlook.com>
2019-02-14 14:41:55 -0500

    Added a cu test file.

---
+++ test.cu
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+__global__
+void saxpy(int n, float a, float *x, float *y)
+{
+	int i = blockIdx.x*blockDim.x + threadIdx.x;
+	if (i < n) y[i] = a*x[i] + y[i];
+}
+
+int main(void)
+{
+	int N = 1<<20;
+	float *x, *y, *d_x, *d_y;
+	x = (float*)malloc(N*sizeof(float));
+	y = (float*)malloc(N*sizeof(float));
+
+	cudaMalloc(&d_x, N*sizeof(float));
+	cudaMalloc(&d_y, N*sizeof(float));
+	
+	saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
+
+	cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+
+	float maxError = 0.0f;
+	for(int i = 0; i < N; i++)
+		maxError = max(maxError, abs(y[i]-4.0f));
+	printf("Max error: %f\n", maxError);
+
+	cudaFree(d_x);
+	cudaFree(d_y);
+	free(x);
+	free(y);
+}
